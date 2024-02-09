dd41289618ee09278cfb9a3758917e015ac9d6a9
Hwangdooyeon <yueuch102@naver.com>
2024-02-09 14:19:00 +0900

    dadok

---
+++ src/main/java/com/dadok/dadok/DadokApplication.java
@@ -0,0 +1,13 @@
+package com.dadok.dadok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadokApplication.class, args);
+	}
+
+}
